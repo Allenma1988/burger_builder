6b5d94bf47b4d2b958bb82c91b4b1eea1245857a
allenma1988 <allenma1988@gmail.com>
2019-10-31 15:29:49 -0700

    Updates

---
+++ precache-manifest.583dfa52025d22e746658f40091e92fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359ff25ebeafcb2a703b8e83a34dd26e",
+    "url": "/burger_builder/index.html"
+  },
+  {
+    "revision": "2dba7861b6624ef174f3",
+    "url": "/burger_builder/static/css/main.4b5c8730.chunk.css"
+  },
+  {
+    "revision": "0ced8ed0c978b5d5b983",
+    "url": "/burger_builder/static/js/2.993e2ff8.chunk.js"
+  },
+  {
+    "revision": "2dba7861b6624ef174f3",
+    "url": "/burger_builder/static/js/main.dc54432c.chunk.js"
+  },
+  {
+    "revision": "0c81f4ad294ef350ea95",
+    "url": "/burger_builder/static/js/runtime-main.a6ebb7c2.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
